a098d57847aaf28afbb091d7cbe96941b41c2e35
brunzze <oolslund@icloud.com>
2023-11-15 13:08:35 +0100

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -8,13 +8,14 @@ addLayer("l", {
     }},
     color: "#4BDC13",
     requires: new Decimal(1), // Can be a function that takes requirement increases into account
-    resource: "Lame points", // Name of prestige currency
+    resource: "Lame", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
+        if (hasUpgrade('l', 13)) mult = mult.times(upgradeEffect('l', 13))
         return mult
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
@@ -22,7 +23,7 @@ addLayer("l", {
     },
     row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
-        {key: "l", description: "L: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+        {key: "l", description: "L: Reset for Lame", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
     upgrades: {
         11: {
@@ -32,10 +33,20 @@ addLayer("l", {
         },
         12: {
             title: "More Fragments",
-            description: "Multiplies Fragment Gain Based On Lame Points",
+            description: "Multiplies Fragment Gain Based On Lame",
             cost: new Decimal(2),
             effect() {
                 return player[this.layer].points.add(1).pow(0.5)
+            },
+            effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, // Add formatting to the effect
+            unlocked(){return hasUpgrade(this.layer, 11)}
+        },
+        13: {
+            title: "Even More Fragments",
+            description: "Multiplies Lame Gain Based On Fragments",
+            cost: new Decimal(5),
+            effect() {
+                return player.points.add(1).pow(0.15)
             },
             effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, // Add formatting to the effect
             unlocked(){return hasUpgrade(this.layer, 11)}
